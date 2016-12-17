554e03db3f232177af9e87984a9177d8ef71945f
Will Usher <will@sci.utah.edu>
2016-12-16 18:19:34 -0600

    cleanup old bash scripts and fix makefile

--- mandelbrot/lmod_set_env.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Sample run script to setup environment variables for the spawned
-# Charm processes on a module based system
-
-module restore
-
-# Run the rest of the arguments, which will be the program followed by its args
-$*
-
