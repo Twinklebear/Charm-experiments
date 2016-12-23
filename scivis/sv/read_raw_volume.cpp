d8b37ce3a7a07a7c526184312541ee4a88db48f7
Will Usher <willusher.life@gmail.com>
2016-12-23 14:10:19 -0800

    Adding a makefile for a separate lib
    This lib will provide the various non-Charm specific scivis code to use
    in the app.

---
+++ scivis/sv/read_raw_volume.cpp
@@ -0,0 +1,12 @@
+#include <fstream>
+#include <string>
+#include <iostream>
+
+namespace sv {
+
+void read_raw_volume(const std::string &fname) {
+	std::cout << "reading volume from " << fname << "\n";
+}
+
+}
+
