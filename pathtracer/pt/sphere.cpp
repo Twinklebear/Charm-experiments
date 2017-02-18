f60a1bc49d4e6a7a64ce97598abf9c29c1754e06
Will Usher <willusher.life@gmail.com>
2017-02-18 13:59:50 -0700

    Actually account for sphere center pos

--- pathtracer/pt/sphere.cpp
+++ pathtracer/pt/sphere.cpp
@@ -1,13 +1,15 @@
 #include <cmath>
+#include <glm/ext.hpp>
 #include "sphere.h"
 
 namespace pt {
 
 Sphere::Sphere(const glm::vec3 &center, const float radius) : center(center), radius(radius) {}
 bool Sphere::intersect(Ray &ray, DifferentialGeometry &dg) const {
-	const float a = glm::dot(ray.dir, ray.dir);
-	const float b = 2.0 * glm::dot(ray.dir, ray.origin);
-	const float c = glm::dot(ray.origin, ray.origin) - radius * radius;
+	const float a = glm::length2(ray.dir);
+	const glm::vec3 oc = ray.origin - center;
+	const float b = 2.0 * glm::dot(ray.dir, oc);
+	const float c = glm::length2(oc) - radius * radius;
 	const float discrim = b * b - 4.0 * a * c;
 	if (discrim > 0.f){
 		float t = (-b - std::sqrt(discrim)) / (2.0 * a);
