c8729448b60417ca67b39bd6fd90abd8b2bf720e
Will Usher <willusher.life@gmail.com>
2017-02-19 21:03:41 -0700

    Add ray depth member

--- pathtracer/pt/ray.cpp
+++ pathtracer/pt/ray.cpp
@@ -3,7 +3,7 @@
 namespace pt {
 
 Ray::Ray(const glm::vec3 &origin, const glm::vec3 &dir, const float t_min, const float t_max)
-	: origin(origin), dir(dir), t_min(t_min), t_max(t_max)
+	: origin(origin), dir(dir), t_min(t_min), t_max(t_max), depth(0)
 {}
 glm::vec3 Ray::at(const float t) const {
 	return origin + dir * t;
--- pathtracer/pt/ray.h
+++ pathtracer/pt/ray.h
@@ -8,6 +8,7 @@ namespace pt {
 struct Ray {
 	glm::vec3 origin, dir;
 	float t_min, t_max;
+	int depth;
 
 	Ray(const glm::vec3 &origin, const glm::vec3 &dir, const float t_min = 0.f,
 			const float t_max = std::numeric_limits<float>::infinity());
