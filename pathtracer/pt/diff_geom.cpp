c232f93d388220085a1910ceef2503d6c749ff07
Will Usher <willusher.life@gmail.com>
2017-02-19 21:04:04 -0700

    Add tangent and bitangent + orthnormalize and shading space methods

---
+++ pathtracer/pt/diff_geom.cpp
@@ -0,0 +1,16 @@
+#include "diff_geom.h"
+
+namespace pt {
+
+void DifferentialGeometry::orthonormalize() {
+	normal = glm::normalize(normal);
+	bitangent = glm::normalize(bitangent);
+	tangent = glm::cross(normal, bitangent);
+	bitangent = glm::cross(tangent, normal);
+}
+glm::vec3 DifferentialGeometry::to_shading(const glm::vec3 &v) const {
+	return glm::vec3(glm::dot(v, bitangent), glm::dot(v, tangent), glm::dot(v, normal));
+}
+
+}
+
--- pathtracer/pt/diff_geom.h
+++ pathtracer/pt/diff_geom.h
@@ -6,8 +6,12 @@ namespace pt {
 
 struct DifferentialGeometry {
 	glm::vec3 point, normal;
-	glm::vec3 tangent, bitangent;
-	// pointer to the material
+	glm::vec3 bitangent, tangent;
+	// todo: pointer to the material
+
+	// Make sure the shading space coordinate frame is orthonormal
+	void orthonormalize();
+	glm::vec3 to_shading(const glm::vec3 &v) const;
 };
 
 }
