c6cc9d9fbe475052636ee6df96d62566439f0194
Will Usher <willusher.life@gmail.com>
2017-02-20 22:12:50 -0700

    Track hit brdf in diff geom

--- pathtracer/pt/diff_geom.cpp
+++ pathtracer/pt/diff_geom.cpp
@@ -11,6 +11,11 @@ void DifferentialGeometry::orthonormalize() {
 glm::vec3 DifferentialGeometry::to_shading(const glm::vec3 &v) const {
 	return glm::vec3(glm::dot(v, bitangent), glm::dot(v, tangent), glm::dot(v, normal));
 }
+glm::vec3 DifferentialGeometry::from_shading(const glm::vec3 &v) const {
+	return glm::vec3(bitangent.x * v.x + tangent.x * v.y + normal.x * v.z,
+			bitangent.y * v.x + tangent.y * v.y + normal.y * v.z,
+			bitangent.z * v.x + tangent.z * v.y + normal.z * v.z);
+}
 
 }
 
--- pathtracer/pt/diff_geom.h
+++ pathtracer/pt/diff_geom.h
@@ -1,17 +1,19 @@
 #pragma once
 
 #include <glm/glm.hpp>
+#include "brdf.h"
 
 namespace pt {
 
 struct DifferentialGeometry {
 	glm::vec3 point, normal;
 	glm::vec3 bitangent, tangent;
-	// todo: pointer to the material
+	const BxDF *brdf = nullptr;
 
 	// Make sure the shading space coordinate frame is orthonormal
 	void orthonormalize();
 	glm::vec3 to_shading(const glm::vec3 &v) const;
+	glm::vec3 from_shading(const glm::vec3 &v) const;
 };
 
 }
