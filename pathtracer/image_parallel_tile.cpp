ea37de91e9448201db2e7c6be21db00c08149c0b
Will Usher <willusher.life@gmail.com>
2017-03-08 10:05:52 -0700

    Change scene to box with one light, seems like there's a material bug

--- pathtracer/image_parallel_tile.cpp
+++ pathtracer/image_parallel_tile.cpp
@@ -40,7 +40,7 @@ void ImageParallelTile::render() {
 	const uint64_t start_y = thisIndex.y * TILE_H;
 	const pt::Camera camera(scene->cam_pos, scene->cam_target, scene->cam_up, 65.0, IMAGE_W, IMAGE_H);
 
-	std::shared_ptr<pt::BxDF> lambertian = std::make_shared<pt::Lambertian>(glm::vec3(0.1, 0.1, 0.8));
+	std::shared_ptr<pt::BxDF> lambertian_blue = std::make_shared<pt::Lambertian>(glm::vec3(0.1, 0.1, 0.8));
 	std::shared_ptr<pt::BxDF> lambertian_white = std::make_shared<pt::Lambertian>(glm::vec3(0.8));
 	std::shared_ptr<pt::BxDF> lambertian_red = std::make_shared<pt::Lambertian>(glm::vec3(0.8, 0.1, 0.1));
 	std::shared_ptr<pt::BxDF> reflective = std::make_shared<pt::SpecularReflection>(glm::vec3(0.8));
@@ -50,14 +50,17 @@ void ImageParallelTile::render() {
 	uint8_t *tile = new uint8_t[TILE_W * TILE_H * 3];
 	const pt::PathIntegrator integrator(glm::vec3(0.05), pt::Scene({
 			std::make_shared<pt::Sphere>(glm::vec3(0), 1.0, reflective),
-			std::make_shared<pt::Sphere>(glm::vec3(0.25, 0.7, 1.0), 0.25, lambertian),
-			std::make_shared<pt::Sphere>(glm::vec3(0), 8, lambertian),
+			std::make_shared<pt::Sphere>(glm::vec3(1.0, 0.7, 1.0), 0.25, lambertian_blue),
 			std::make_shared<pt::Sphere>(glm::vec3(-1, -0.75, 1.2), 0.5, lambertian_red),
-			std::make_shared<pt::Plane>(glm::vec3(0, -1, 0), glm::vec3(0, 1, 0), lambertian_white)
+			// Walls
+			std::make_shared<pt::Plane>(glm::vec3(0, -1, 0), glm::vec3(0, 1, 0), lambertian_white),
+			std::make_shared<pt::Plane>(glm::vec3(0, 2, 0), glm::vec3(0, -1, 0), lambertian_white),
+			std::make_shared<pt::Plane>(glm::vec3(-1.5, 0, 0), glm::vec3(1, 0, 0), lambertian_white),
+			std::make_shared<pt::Plane>(glm::vec3(1.5, 0, 0), glm::vec3(-1, 0, 0), lambertian_white),
+			std::make_shared<pt::Plane>(glm::vec3(0, 0, -2), glm::vec3(0, 0, 1), lambertian_white)
 		},
 		{
-			std::make_shared<pt::PointLight>(glm::vec3(1, 1.5, 1.5), glm::vec3(1.0)),
-			std::make_shared<pt::PointLight>(glm::vec3(-0.5, 1.5, 1.5), glm::vec3(0.7))
+			std::make_shared<pt::PointLight>(glm::vec3(0, 1.5, 0), glm::vec3(0.9)),
 		}
 	));
 	std::mt19937 rng{std::random_device()()};
@@ -66,14 +69,15 @@ void ImageParallelTile::render() {
 	for (uint64_t i = 0; i < TILE_H; ++i) {
 		for (uint64_t j = 0; j < TILE_W; ++j) {
 		glm::vec3 color(0);
+		const uint64_t spp = 32;
 		// TODO: Samples should be in multiple passes like in the aobench render
-		for (uint64_t sp = 0; sp < 32; ++sp) {
+		for (uint64_t sp = 0; sp < spp; ++sp) {
 			const float px = j + start_x;
 			const float py = i + start_y;
 			pt::Ray ray = camera.generate_ray(px, py, {real_distrib(rng), real_distrib(rng)});
 			color += integrator.integrate(ray);
 		}
-		color = linear_to_srgb(color / 32.f);
+		color = linear_to_srgb(color / static_cast<float>(spp));
 			for (size_t c = 0; c < 3; ++c) {
 				tile[(i * TILE_W + j) * 3 + c] = glm::clamp(color[c] * 255.f, 0.f, 255.f);
 			}
