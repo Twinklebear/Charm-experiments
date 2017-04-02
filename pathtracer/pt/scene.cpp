3ffa6983d0836c43a9ed8e43ea606e7d43fc46de
Will Usher <willusher.life@gmail.com>
2017-04-01 23:49:35 -0600

    update to glm 0.9.8.4

--- pathtracer/pt/scene.cpp
+++ pathtracer/pt/scene.cpp
@@ -1,3 +1,4 @@
+#include <numeric>
 #include <algorithm>
 #include "scene.h"
 
--- pathtracer/pup_operators.h
+++ pathtracer/pup_operators.h
@@ -5,7 +5,7 @@
 #include "pt/pt.h"
 
 template<typename T>
-inline void operator|(PUP::er &p, glm::detail::tvec3<T, (glm::precision)0> &v) {
+inline void operator|(PUP::er &p, glm::tvec3<T, (glm::precision)0> &v) {
 	p | v.x;
 	p | v.y;
 	p | v.z;
