97dab47d082e8e7b25371d30b151d0e3be2cd1be
Will Usher <willusher.life@gmail.com>
2017-05-17 22:17:05 -0500

    starting on a bit of cleanup, mostly just more todo notes

--- pathtracer/pt/bvh.cpp
+++ pathtracer/pt/bvh.cpp
@@ -10,9 +10,11 @@
 #include "geometry/bbox.h"
 #include "accelerators/bvh.h"
 
-BVH::GeomInfo::GeomInfo(int i, const BBox &b) : geom_idx(i), center(b.lerp(0.5, 0.5, 0.5)), bounds(b){}
+namespace pt {
 
-BVH::BuildNode::BuildNode(int geom_offset, int ngeom, const BBox &bounds)
+BVH::GeomInfo::GeomInfo(size_t i, const BBox &b) : geom_idx(i), center(b.lerp(0.5, 0.5, 0.5)), bounds(b){}
+
+BVH::BuildNode::BuildNode(size_t geom_offset, size_t ngeom, const BBox &bounds)
 	: children({nullptr, nullptr}), geom_offset(geom_offset), ngeom(ngeom), bounds(bounds)
 {}
 BVH::BuildNode::BuildNode(AXIS split, std::unique_ptr<BuildNode> a, std::unique_ptr<BuildNode> b)
@@ -20,10 +22,16 @@ BVH::BuildNode::BuildNode(AXIS split, std::unique_ptr<BuildNode> a, std::unique_
 	bounds(children[0]->bounds.box_union(children[1]->bounds)), split(split)
 {}
 
-BVH::SAHBucket::SAHBucket() : count(0){}
+// Bucket used for SAH split method
+struct SAHBucket {
+	size_t count;
+	BBox bounds;
 
-BVH::BVH(const std::vector<Geometry*> &geom, SPLIT_METHOD split, unsigned max_geom)
-	: split(split), max_geom(std::min(256u, max_geom))
+	SAHBucket() : count(0) {}
+};
+
+BVH::BVH(const std::vector<Geometry*> &geom, unsigned max_geom)
+	: max_geom(std::min(256u, max_geom))
 {
 	for (Geometry *g : geom){
 		g->refine(geometry);
@@ -41,7 +49,7 @@ BVH::BVH(const std::vector<Geometry*> &geom, SPLIT_METHOD split, unsigned max_ge
 
 	std::vector<Geometry*> ordered_geom;
 	ordered_geom.reserve(geometry.size());
-	int total_nodes = 0;
+	size_t total_nodes = 0;
 	std::unique_ptr<BuildNode> root = build(build_geom, ordered_geom, 0, geometry.size(), total_nodes);
 	//Our BVH structure refers to the ordered geometry so swap out our unordered list for
 	//the correctly ordered one
@@ -49,7 +57,7 @@ BVH::BVH(const std::vector<Geometry*> &geom, SPLIT_METHOD split, unsigned max_ge
 
 	//Recursively flatten the tree for faster traversal
 	flat_nodes.resize(total_nodes);
-	uint32_t offset = 0;
+	size_t offset = 0;
 	flatten_tree(root, offset);
 }
 BBox BVH::bounds() const {
@@ -63,6 +71,10 @@ bool BVH::intersect(Ray &r, DifferentialGeometry &diff_geom) const {
 	Vector inv_dir{1 / r.d.x, 1 / r.d.y, 1 / r.d.z};
 	std::array<int, 3> neg_dir = {inv_dir.x < 0, inv_dir.y < 0, inv_dir.z < 0};
 	//Stack of nodes to be visited and the current node being visited. todo_offset is the top of stack
+	// TODO: Replace with Afra and Szirmay-Kalos stackless bitstring traversal
+	// make possible to get this string out so we can traverse regions, not necessarily
+	// geometry we're interesting. On a local node we'd use Embree, at the data distribution
+	// level (and maybe 1 or 2 down to local data) we'd use this BVH
 	std::array<int, 64> todo;
 	int todo_offset = 0, current = 0;
 	//Step through the BVH visiting the current node and pushing on nodes that need to be visited
@@ -107,8 +119,8 @@ bool BVH::intersect(Ray &r, DifferentialGeometry &diff_geom) const {
 	}
 	return hit;
 }
-std::unique_ptr<BVH::BuildNode> BVH::build(std::vector<GeomInfo> &build_geom, std::vector<Geometry*> &ordered_geom,
-	int start, int end, int &total_nodes)
+std::unique_ptr<BVH::BuildNode> BVH::build(std::vector<GeomInfo> &build_geom,
+		std::vector<Geometry*> &ordered_geom, size_t start, size_t end, size_t &total_nodes)
 {
 	++total_nodes;
 	//Find total bounds for the geometry we're trying to store
@@ -116,7 +128,7 @@ std::unique_ptr<BVH::BuildNode> BVH::build(std::vector<GeomInfo> &build_geom, st
 	for (int i = start; i < end; ++i){
 		box = box.box_union(build_geom[i].bounds);
 	}
-	int ngeom = end - start;
+	const size_t ngeom = end - start;
 	//Build and return a leaf node for the geometry
 	if (ngeom == 1){
 		return build_leaf(build_geom, ordered_geom, start, end, box);
@@ -125,12 +137,12 @@ std::unique_ptr<BVH::BuildNode> BVH::build(std::vector<GeomInfo> &build_geom, st
 	//Figure out which axis to split on by bounding the various geometry's centroids
 	//and picking the one with the most variation
 	BBox centroids;
-	for (int i = start; i < end; ++i){
+	for (size_t i = start; i < end; ++i){
 		centroids = centroids.box_union(build_geom[i].center);
 	}
 	AXIS axis = centroids.max_extent();
 	//We can now partition the primitives along this axis
-	int mid = (start + end) / 2;
+	size_t mid = (start + end) / 2;
 	//If all the geometry's centers are on the same point we can't partition
 	if (centroids.max[axis] == centroids.min[axis]){
 		//Check that we can fit all the geometry into a single leaf node, if not we need
@@ -144,109 +156,82 @@ std::unique_ptr<BVH::BuildNode> BVH::build(std::vector<GeomInfo> &build_geom, st
 				build(build_geom, ordered_geom, mid, end, total_nodes));
 		}
 	}
-	//Partition the primitives base on split method chosen
-	switch (split){
-		case SPLIT_METHOD::MIDDLE: {
-			float mid_pt = 0.5f * (centroids.min[axis] + centroids.max[axis]);
-			auto mid_ptr = std::partition(build_geom.begin() + start, build_geom.begin() + end,
-				[axis, mid_pt](const GeomInfo &g){
-					return g.center[axis] < mid_pt;
-				});
-			mid = std::distance(build_geom.begin(), mid_ptr);
-			//This technique can fail if the geometry bounds overlap a lot, in which case
-			//fall through to equal split method
-			if (mid != start && mid != end){
-				break;
-			}
-		}
-		case SPLIT_METHOD::EQUAL: {
-			mid = (start + end) / 2;
-			std::nth_element(build_geom.begin() + start, build_geom.begin() + mid,
+	// Partition the primitives based on the surface area heuristic
+	// If there's only a few primitives just use EQUAL and break
+	if (ngeom < 5){
+		mid = (start + end) / 2;
+		std::nth_element(build_geom.begin() + start, build_geom.begin() + mid,
 				build_geom.begin() + end,
 				[axis](const GeomInfo &a, const GeomInfo &b){
-					return a.center[axis] < b.center[axis];
+				return a.center[axis] < b.center[axis];
 				});
-			break;
-		}
-		case SPLIT_METHOD::SAH: {
-			//If there's only a few primitives just use EQUAL and break
-			if (ngeom < 5){
-				mid = (start + end) / 2;
-				std::nth_element(build_geom.begin() + start, build_geom.begin() + mid,
-					build_geom.begin() + end,
-					[axis](const GeomInfo &a, const GeomInfo &b){
-						return a.center[axis] < b.center[axis];
-					});
-				break;
-			}
-			//We're just going to consider the possibilities of splitting along 12 bucket boundaries
-			std::array<SAHBucket, 12> buckets;
-			//Place the various geometry we're partitioning into the appropriate bucket, scale
-			//the position along the axis into a bucket index
-			for (int i = start; i < end; ++i){
-				int b = (build_geom[i].center[axis] - centroids.min[axis])
-					/ (centroids.max[axis] - centroids.min[axis]) * buckets.size();
-				b = b == buckets.size() ? b - 1 : b;
-				++buckets[b].count;
-				buckets[b].bounds = buckets[b].bounds.box_union(build_geom[i].bounds);
-			}
-			//Use the SAH to compute the costs of splitting at each bucket except the last
-			std::array<float, 11> cost;
-			for (int i = 0; i < cost.size(); ++i){
-				SAHBucket left, right;
-				for (int j = 0; j <= i; ++j){
-					left.bounds = left.bounds.box_union(buckets[j].bounds);
-					left.count += buckets[j].count;
-				}
-				for (int j = i + 1; j < buckets.size(); ++j){
-					right.bounds = right.bounds.box_union(buckets[j].bounds);
-					right.count += buckets[j].count;
-				}
-				//cost: cost of traversel + (cost of hitting in left + cost of hitting in right) / total area of node
-				cost[i] = .125f + (left.count * left.bounds.surface_area()
-					+ right.count * right.bounds.surface_area()) / box.surface_area();
-			}
-			//Find the lowest cost split we can make here
-			auto min_cost = std::min_element(cost.begin(), cost.end());
-			int min_cost_idx = std::distance(cost.begin(), min_cost);
-			//If we're forced to split by the amount of geometry here or it's cheaper to split then do so
-			if (ngeom > max_geom || *min_cost < ngeom){
-				//Partition the geometry about the splitting bucket
-				auto mid_ptr = std::partition(build_geom.begin() + start, build_geom.begin() + end,
-					[min_cost_idx, axis, centroids](const GeomInfo &g){
-						int b = (g.center[axis] - centroids.min[axis])
-							/ (centroids.max[axis] - centroids.min[axis]) * 12;
-						b = b == 12 ? b - 1 : b;
-						return b <= min_cost_idx;
-					});
-				mid = std::distance(build_geom.begin(), mid_ptr);
-			}
-			else {
-				return build_leaf(build_geom, ordered_geom, start, end, box);
-			}
-			break;
-		}
+		break;
 	}
+	//We're just going to consider the possibilities of splitting along 12 bucket boundaries
+	std::array<SAHBucket, 12> buckets;
+	//Place the various geometry we're partitioning into the appropriate bucket, scale
+	//the position along the axis into a bucket index
+	for (size_t i = start; i < end; ++i){
+		size_t b = (build_geom[i].center[axis] - centroids.min[axis])
+			/ (centroids.max[axis] - centroids.min[axis]) * buckets.size();
+		b = b == buckets.size() ? b - 1 : b;
+		++buckets[b].count;
+		buckets[b].bounds = buckets[b].bounds.box_union(build_geom[i].bounds);
+	}
+	//Use the SAH to compute the costs of splitting at each bucket except the last
+	std::array<float, 11> cost;
+	for (size_t i = 0; i < cost.size(); ++i){
+		SAHBucket left, right;
+		for (size_t j = 0; j <= i; ++j){
+			left.bounds = left.bounds.box_union(buckets[j].bounds);
+			left.count += buckets[j].count;
+		}
+		for (size_t j = i + 1; j < buckets.size(); ++j){
+			right.bounds = right.bounds.box_union(buckets[j].bounds);
+			right.count += buckets[j].count;
+		}
+		//cost: cost of traversel + (cost of hitting in left + cost of hitting in right) / total area of node
+		cost[i] = .125f + (left.count * left.bounds.surface_area()
+				+ right.count * right.bounds.surface_area()) / box.surface_area();
+	}
+	//Find the lowest cost split we can make here
+	const auto min_cost = std::min_element(cost.begin(), cost.end());
+	const size_t min_cost_idx = std::distance(cost.begin(), min_cost);
+	//If we're forced to split by the amount of geometry here or it's cheaper to split then do so
+	if (ngeom > max_geom || *min_cost < ngeom){
+		//Partition the geometry about the splitting bucket
+		auto mid_ptr = std::partition(build_geom.begin() + start, build_geom.begin() + end,
+				[min_cost_idx, axis, centroids](const GeomInfo &g){
+				size_t b = (g.center[axis] - centroids.min[axis])
+				/ (centroids.max[axis] - centroids.min[axis]) * 12;
+				b = b == 12 ? b - 1 : b;
+				return b <= min_cost_idx;
+				});
+		mid = std::distance(build_geom.begin(), mid_ptr);
+	} else {
+		return build_leaf(build_geom, ordered_geom, start, end, box);
+	}
+
 	assert(start != mid && mid != end);
 	return std::make_unique<BuildNode>(axis,
 		build(build_geom, ordered_geom, start, mid, total_nodes),
 		build(build_geom, ordered_geom, mid, end, total_nodes));
 }
-std::unique_ptr<BVH::BuildNode> BVH::build_leaf(std::vector<GeomInfo> &build_geom, std::vector<Geometry*> &ordered_geom,
-	int start, int end, const BBox &box)
+std::unique_ptr<BVH::BuildNode> BVH::build_leaf(std::vector<GeomInfo> &build_geom,
+		std::vector<Geometry*> &ordered_geom, size_t start, size_t end, const BBox &box)
 {
-	int ngeom = end - start;
-	//Store the offset to this leaf's geometry then push it on
-	int geom_offset = ordered_geom.size();
-	for (int i = start; i < end; ++i){
+	const size_t ngeom = end - start;
+	// Store the offset to this leaf's geometry then push it on
+	const size_t geom_offset = ordered_geom.size();
+	for (size_t i = start; i < end; ++i){
 		ordered_geom.push_back(geometry[build_geom[i].geom_idx]);
 	}
 	return std::make_unique<BuildNode>(geom_offset, ngeom, box);
 }
-uint32_t BVH::flatten_tree(const std::unique_ptr<BuildNode> &node, uint32_t &offset){
+size_t BVH::flatten_tree(const std::unique_ptr<BuildNode> &node, size_t &offset){
 	FlatNode &fnode = flat_nodes[offset];
 	fnode.bounds = node->bounds;
-	uint32_t node_offset = offset++;
+	const size_t node_offset = offset++;
 	//If the node has geometry we're creating a leaf
 	if (node->ngeom > 0){
 		fnode.geom_offset = node->geom_offset;
@@ -261,34 +246,6 @@ uint32_t BVH::flatten_tree(const std::unique_ptr<BuildNode> &node, uint32_t &off
 	}
 	return node_offset;
 }
-bool BVH::fast_box_intersect(const BBox &bounds, const Ray &r, const Vector &inv_dir, const std::array<int, 3> &neg_dir) const {
-	//Check X & Y intersection
-	float tmin = (bounds[neg_dir[0]].x - r.o.x) * inv_dir.x;
-	float tmax = (bounds[1 - neg_dir[0]].x - r.o.x) * inv_dir.x;
-	float tymin = (bounds[neg_dir[1]].y - r.o.y) * inv_dir.y;
-	float tymax = (bounds[1 - neg_dir[1]].y - r.o.y) * inv_dir.y;
-	if (tmin > tymax || tymin > tmax){
-		return false;
-	}
-	if (tymin > tmin){
-		tmin = tymin;
-	}
-	if (tymax < tmax){
-		tmax = tymax;
-	}
 
-	//Check Z intersection
-	float tzmin = (bounds[neg_dir[2]].z - r.o.z) * inv_dir.z;
-	float tzmax = (bounds[1 - neg_dir[2]].z - r.o.z) * inv_dir.z;
-	if (tmin > tzmax || tzmin > tmax){
-		return false;
-	}
-	if (tzmin > tmin){
-		tmin = tzmin;
-	}
-	if (tzmax < tmax){
-		tmax = tzmax;
-	}
-	return tmin < r.max_t && tmax > r.min_t;
 }
 
--- pathtracer/pt/bvh.h
+++ pathtracer/pt/bvh.h
@@ -3,133 +3,107 @@
 #include <memory>
 #include <vector>
 #include <array>
-#include "linalg/ray.h"
-#include "linalg/util.h"
-#include "geometry/bbox.h"
-#include "geometry/differential_geometry.h"
+#include "ray.h"
+#include "bbox.h"
+#include "geometry.h"
 
 // TODO: Rewrite to use Afra & Szirmay-Kalos's stackless MBVH2 traversal
 // and clean up code to integrate into libpt
 
 namespace pt {
 
-/*
- * Different methods that can be used to partition the space
- * when building the BVH
- */
-enum class SPLIT_METHOD { MIDDLE, EQUAL, SAH };
-
-class Geometry;
-
-/*
- * A BVH built off of the one introduced in PBR
+/* A BVH2 built off of the one introduced in PBR and
+ * modified to use Afta and Szirmay-Kalos's stackless traversal
+ * algorithm.
  */
 class BVH {
-	//Information about some geometry being place in the BVH
+	// Information about some geometry being place in the BVH
 	struct GeomInfo {
-		//The index of the geometry in the BVH's geometry vector
-		int geom_idx;
-		//Center of the geometry's bounding box
+		// The index of the geometry in the BVH's geometry vector
+		size_t geom_idx;
+		// Center of the geometry's bounding box
 		Point center;
 		BBox bounds;
 
-		GeomInfo(int i, const BBox &b);
+		GeomInfo(size_t i, const BBox &b);
 	};
-	//Node used when constructing the BVH tree
+
+	// Node used when constructing the BVH tree
 	struct BuildNode {
-		//Node's children, null if a leaf node
+		// Node's children, null if a leaf node. TODO: Also track sibling and parent
 		std::array<std::unique_ptr<BuildNode>, 2> children;
-		//Offset to the first geometry in this node and # of geometry in it
-		//ngeom = 0 if this is an interior node
-		int geom_offset, ngeom;
+		/* Offset to the first geometry in this node and # of geometry in it
+		 * ngeom = 0 if this is an interior node
+		 */
+		size_t geom_offset, ngeom;
 		BBox bounds;
-		//Which axis was split to form this node's children
+		// Which axis was split to form this node's children
 		AXIS split;
 
 		/*
 		 * Construct a leaf node
 		 */
-		BuildNode(int geom_offset, int ngeom, const BBox &bounds);
+		BuildNode(size_t geom_offset, size_t ngeom, const BBox &bounds);
 		/*
 		 * Construct an interior node, where a & b are the node's children
 		 */
 		BuildNode(AXIS split, std::unique_ptr<BuildNode> a, std::unique_ptr<BuildNode> b);
 	};
-	/*
-	 * Nodes used to store the final flattened BVH structure, the first child
+
+	/* Nodes used to store the final flattened BVH structure, the first child
 	 * is right after the parent but the second child is at some offset further down
 	 */
 	struct FlatNode {
 		BBox bounds;
+		// TODO: Store sibling and parent info as well
 		union {
-			//Used for leaves to locate geometry
-			int geom_offset;
-			//Used for interiors to locate second child
-			int second_child;
+			// Used for leaves to locate geometry
+			size_t geom_offset;
+			// Used for interiors to locate second child
+			size_t second_child;
 		};
 		//Number of geometry stored in this node, 0 if interior
 		uint16_t ngeom;
 		uint16_t axis;
 	};
-	//Bucket used for SAH split method
-	struct SAHBucket {
-		int count;
-		BBox bounds;
 
-		SAHBucket();
-	};
-
-	SPLIT_METHOD split;
-	unsigned max_geom;
+	size_t max_geom;
 	//The geometry being stored in this BVH
 	std::vector<Geometry*> geometry;
 	//The final flatted BVH structure
 	std::vector<FlatNode> flat_nodes;
 
 public:
-	/*
-	 * Construct the BVH to create a hierarchy of the refined geometry passed in
+	/* Construct the BVH to create a hierarchy of the refined geometry passed in
 	 * using the desird split method. max_geom specifies the maximum geometry that
 	 * can be stored per node, default is 128, max is 256
 	 * The defaults for the empty constructor will build an empty BVH
 	 */
 	BVH(const std::vector<Geometry*> &geom = std::vector<Geometry*>{},
-		SPLIT_METHOD split = SPLIT_METHOD::SAH, unsigned max_geom = 128);
-	/*
-	 * Get the bounds for the BVH
-	 */
+		size_t max_geom = 128);
+	// Get the bounds for the BVH
 	BBox bounds() const;
-	/*
-	 * Perform an intersection test on the geometry stored in the BVH
-	 */
+	// Perform an intersection test on the geometry stored in the BVH
 	bool intersect(Ray &ray, DifferentialGeometry &diff_geom) const;
 
 private:
-	/*
-	 * Construct a subtree of the BVH for the build_geom from [start, end)
+	/* Construct a subtree of the BVH for the build_geom from [start, end)
 	 * and return the root of this subtree. The geometry is placed in the child
 	 * nodes is partitioned into ordered geom for easier look up later
 	 * Also returns the total nodes in this subtree, for use later when flattening
 	 * the BVH
 	 */
 	std::unique_ptr<BuildNode> build(std::vector<GeomInfo> &build_geom, std::vector<Geometry*> &ordered_geom,
-		int start, int end, int &total_nodes);
-	/*
-	 * Build a leaf node in the tree using the geometry passed and push the ordered geometry for the
+		size_t start, size_t end, size_t &total_nodes);
+	/* Build a leaf node in the tree using the geometry passed and push the ordered geometry for the
 	 * leaf into ordered_geom
 	 */
 	std::unique_ptr<BuildNode> build_leaf(std::vector<GeomInfo> &build_geom, std::vector<Geometry*> &ordered_geom,
-		int start, int end, const BBox &box);
-	/*
-	 * Recursively flatten the BVH tree into the flat nodes vector
+		size_t start, size_t end, const BBox &box);
+	/* Recursively flatten the BVH tree into the flat nodes vector
 	 * offset tracks the current offset into the flat nodes vector
 	 */
-	uint32_t flatten_tree(const std::unique_ptr<BuildNode> &node, uint32_t &offset);
-	/*
-	 * A specialized fast bbox intersection test for the BVH traversal
-	 * Based on the optimized multiple box test from http://people.csail.mit.edu/amy/papers/box-jgt.pdf
-	 */
-	bool fast_box_intersect(const BBox &bounds, const Ray &r, const Vector &inv_dir, const std::array<int, 3> &neg_dir) const;
+	size_t flatten_tree(const std::unique_ptr<BuildNode> &node, size_t &offset);
 };
 
 }
