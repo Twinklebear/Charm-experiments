f917afef7de574987b3953ef9aabd57937b7b576
Will Usher <willusher.life@gmail.com>
2017-06-03 13:22:29 -0500

    Add ActiveRay struct and IntersectionResult
    ActiveRay bundles up the various info needed to ship the ray around and
    suspend/resume it and so on. The IntersectionResult will be returned by
    renderers from their integrate method to inform the caller what rays (if
    any) should be continued on.

---
+++ pathtracer/pt/integrator.cpp
@@ -0,0 +1,29 @@
+#include "integrator.h"
+
+namespace pt {
+
+ActiveRay::ActiveRay(const Ray &r, const uint64_t owner_id, const uint64_t tile,
+		const uint64_t pixel)
+	: type(PRIMARY), ray(r), owner_id(owner_id), tile(tile), pixel(pixel)
+{}
+ActiveRay* ActiveRay::shadow(const Ray &r, const ActiveRay &parent) {
+	ActiveRay *ar = new ActiveRay(parent);
+	ar->type = SHADOW;
+	ar->ray = r;
+	ar->owner_id = parent.owner_id;
+	ar->tile = parent.tile;
+	ar->pixel = parent.pixel;
+	return ar;
+}
+ActiveRay* ActiveRay::secondary(const Ray &r, const ActiveRay &parent) {
+	ActiveRay *ar = new ActiveRay(parent);
+	ar->type = SECONDARY;
+	ar->ray = r;
+	ar->owner_id = parent.owner_id;
+	ar->tile = parent.tile;
+	ar->pixel = parent.pixel;
+	return ar;
+}
+
+}
+
---
+++ pathtracer/pt/integrator.h
@@ -0,0 +1,56 @@
+#pragma once
+
+#include <memory>
+#include <glm/glm.hpp>
+#include "ray.h"
+
+namespace pt {
+
+enum RAY_TYPE {
+	PRIMARY,
+	SHADOW,
+	SECONDARY
+};
+
+struct BVHTraversalState {
+	size_t current;
+	size_t bitstack;
+
+	BVHTraversalState();
+};
+
+/* A ray being actively traversed through the scene, can be
+ * either a shadow ray or a secondary (continuation) ray. On
+ * a shadow ray the color is the final shaded color for the hit
+ * point, and all the receiving end must do is determine if the
+ * ray hits something before it hits the light. On a secondary ray
+ * the color is the path throughput, for path tracing.
+ *
+ * Once the shadow test has been executed or the secondary ray
+ * terminated the node on which it finishes must report the
+ * shading result back to the owner.
+ */
+struct ActiveRay {
+	RAY_TYPE type;
+	Ray ray;
+	BVHTraversalState traversal;
+	glm::vec3 color;
+	uint64_t owner_id, tile, pixel;
+
+	ActiveRay(const Ray &r, const uint64_t owner_id, const uint64_t tile,
+			const uint64_t pixel);
+	static ActiveRay* shadow(const Ray &r, const ActiveRay &parent);
+	static ActiveRay* secondary(const Ray &r, const ActiveRay &parent);
+};
+
+struct IntersectionResult {
+	// TODO: For the shadow ray, I can compute the entire shading
+	// result that would be sent because all nodes have all the light
+	// information, since they're delta or analytic lights. The only
+	// thing a node doesn't know is if there's some geometry on another
+	// node blocking its shading point from the light.
+	std::unique_ptr<ActiveRay> shadow, secondary;
+};
+
+}
+
